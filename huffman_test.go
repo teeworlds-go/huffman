45077d1975c3ab63ec4915a083e64569c084d6ca
ChillerDragon <ChillerDragon@gmail.com>
2024-06-05 10:19:02 +0800

    Add more tests

--- huffman_test.go
+++ huffman_test.go
@@ -20,3 +20,29 @@ func TestCompressFoo(t *testing.T) {
 		t.Errorf("got %v, wanted %v", got, want)
 	}
 }
+
+func TestCompressHelloWorld(t *testing.T) {
+	huff := Huffman{}
+	huff.init()
+
+	got, err := huff.compress([]byte("hello world"))
+	if err != nil {
+		t.Errorf("failed to compress foo %v", err)
+	}
+
+	want := []byte{174, 149, 19, 92, 9, 87, 194, 22, 177, 86, 220, 218, 34, 56, 185, 18, 156, 168, 184, 1}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
+
+func TestCompressEmpty(t *testing.T) {
+	huff := Huffman{}
+	huff.init()
+
+	_, err := huff.compress([]byte{})
+	if err == nil {
+		t.Errorf("Expected error on empty input")
+	}
+}
